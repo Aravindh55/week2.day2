265506f07ac8313219c0595dac1437f21ac7de96
Aravindh55 <91793881+Aravindh55@users.noreply.github.com>
2021-10-11 14:58:31 +0530

    Add files via upload

---
+++ DeleteLead.java
@@ -0,0 +1,46 @@
+package week2.day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DeleteLead {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		WebElement user = driver.findElement(By.id("username"));
+		user.sendKeys("Demosalesmanager");
+		WebElement pass = driver.findElement(By.id("password"));
+		pass.sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//span[text()='Phone']")).click();
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("91");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		String text = driver.findElement(By.linkText("12788")).getText();
+		driver.findElement(By.linkText(text)).click();
+		driver.findElement(By.xpath("//a[@class='subMenuButtonDangerous']")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//input[@name='id']")).sendKeys(text);
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		String text2 = driver.findElement(By.xpath("//div[text()='No records to display']")).getText();
+		if (text2.contains("No records to display")) {
+			System.out.println("verified");
+
+		} else {
+			System.out.println("not verified");
+		}
+		driver.close();
+
+	}
+
+}
