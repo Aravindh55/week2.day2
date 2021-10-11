ef5af2fdac9c43038c1c696c4b6723505863ecc4
Aravindh55 <91793881+Aravindh55@users.noreply.github.com>
2021-10-11 21:17:36 +0530

    Add files via upload

---
+++ EditFields.java
@@ -0,0 +1,35 @@
+package week2.day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class EditFields {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leafground.com/pages/Edit.html");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.id("email")).sendKeys("aravindhraja.ra@gmail.com");
+		driver.findElement(By.xpath("//input[@value='Append ']")).sendKeys("abc");
+		driver.findElement(By.xpath("//input[@value='Append ']")).sendKeys(Keys.TAB);
+		String text = driver.findElement(By.xpath("(//input[@name='username'])[1]")).getText();
+		System.out.println(text);
+		driver.findElement(By.xpath("(//input[@name='username'])[2]")).clear();
+		boolean element= driver.findElement(By.xpath("(//input[@type='text'])[5]")).isEnabled();
+		if (element==true) {
+			System.out.println("the field is enabled");
+			
+		}
+		else {
+			System.out.println("the field is disabled");
+		}
+		
+}
+
+}
