2051986b0a5f00136407c9c75b60e9094b4aa1e5
Aravindh55 <91793881+Aravindh55@users.noreply.github.com>
2021-10-12 22:42:20 +0530

    Add files via upload

---
+++ CheckBoxes.java
@@ -0,0 +1,45 @@
+package week2.day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CheckBoxes {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leafground.com/pages/checkbox.html");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.xpath("(//div[@class='example']//input)[1]")).click();
+		boolean a = driver.findElement(By.xpath("(//div[@class='example']//input)[6]")).isSelected();
+		if (a==true) {
+			System.out.println("selenium is checked");
+		}
+		else {
+			System.out.println("selenium is not checked");
+		}
+		boolean b = driver.findElement(By.xpath("(//div[@class='example']//input)[7]")).isSelected();
+		boolean c = driver.findElement(By.xpath("(//div[@class='example']//input)[8]")).isSelected();
+		if (b==true) {
+			driver.findElement(By.xpath("(//div[@class='example']//input)[7]")).click();
+			
+		}
+		if (c==true) {
+			driver.findElement(By.xpath("(//div[@class='example']//input)[8]")).click();
+			
+		}
+		driver.findElement(By.xpath("(//div[@class='example']//input)[9]")).click();
+		driver.findElement(By.xpath("(//div[@class='example']//input)[10]")).click();
+		driver.findElement(By.xpath("(//div[@class='example']//input)[11]")).click();
+		driver.findElement(By.xpath("(//div[@class='example']//input)[12]")).click();
+		driver.findElement(By.xpath("(//div[@class='example']//input)[13]")).click();
+		
+		
+	}
+
+}
+
